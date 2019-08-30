a224b9925b4678a0420b801fec062a08b7555265
Christophe Bedard <bedard.christophe@gmail.com>
2019-08-30 11:55:55 -0400

    Ignore vimeo links when running htmlproofer

--- scripts/proof.sh
+++ scripts/proof.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bundle exec htmlproofer ./_site --allow-hash-href --http-status-ignore 999
+bundle exec htmlproofer ./_site --allow-hash-href --url-ignore *vimeo.com* --http-status-ignore 999
